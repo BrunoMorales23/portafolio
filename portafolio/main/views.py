08cc6407ae0971a861816f0436f997bf5cd7e63a
Bruno Morales <brunomorales0323@outlook.es>
2025-10-08 17:52:36 -0300

    Experiencia Laboral Page Finished

--- portafolio/main/views.py
+++ portafolio/main/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+request_flush_required = True
 
 def home(request):
-    request.method == "GET"
-    return render(request, "index.html")
+    global request_flush_required
+    if request_flush_required == True:
+        request.session.flush()
+        print("--- Session Flush ---")
+        request_flush_required = False
+    if "section" not in request.session:
+        request.session["section"] = "home"
+
+    section = request.session["section"]
+    print(section)
+
+    if request.method == "POST":
+        section = request.POST.get("action", "home")
+        request.session["section"] = section
+        return redirect("home")
+    
+    return render(request, "index.html", {"section": section})
